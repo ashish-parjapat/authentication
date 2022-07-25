407fa08cb361204019ef535b594b006748597b50
Ashish Kumar <96014503+ashish-parjapat@users.noreply.github.com>
2022-07-25 22:54:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,27 +5,42 @@ const bodyParser = require("body-parser")
 
 const ejs = require("ejs");
 const { default: mongoose } = require('mongoose');
+const session = require('express-session')
+const password = require("passport")
+const passwordLocalMongoose = require("passport-local-mongoose");
+const passport = require('passport');
 const app = express();
 const port = 3000
-var encrypt = require('mongoose-encryption');
+    // var encrypt = require('mongoose-encryption');
 
-require("./conneect")
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+
+}))
+app.use(password.initialize())
+app.use(password.session)
+
+require("./conneect")
 const userSchema = new mongoose.Schema({
     email: String,
+
     password: String,
 })
-const secret = "thisisourlittlesecret";
-userSchema.plugin(encrypt, {
-    secret: secret,
-    encryptedFields: ["password"]
-})
+userSchema.plugin(passwordLocalMongoose)
+
 const Users = new mongoose.model("user", userSchema);
+passport.serializeUser(Users.serializeUser())
+passport.deserializeUser(Users.deserializeUser())
 
 app.get("/", function(req, res) {
     res.render("home")
@@ -39,36 +54,12 @@ app.get("/register", function(req, res) {
 });
 //getting p\assword and email id from user for regiseter \page
 app.post("/register", function(req, res) {
-    const newUser = new Users({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save(function(error) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.render("secrets")
-        }
-    })
+
 
 });
 //checking in the database if correct user id enterimg lthesercert page
 app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-    Users.findOne({ email: username }, function(err, foundUser) {
-        if (err) {
-            console.log(err);
-        } else {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets")
-                } else {
-                    console.log("wrong password")
-                }
-            }
-        }
-    })
+
 })
 
 
